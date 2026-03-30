5322a8851fecdd848e5cb15ed833bd765687dc67
cbarthel01 <cbarthel01@pc-0961.csip.in>
2026-03-30 17:17:13 +0200

    Batiments

--- src/main/java/com/mycompany/mavenproject1/Batiment.java
+++ src/main/java/com/mycompany/mavenproject1/Batiment.java
@@ -10,4 +10,39 @@ package com.mycompany.mavenproject1;
  */
 public class Batiment {
     
+    private int idBatiment;
+    private String typeBatiment;
+    private int nbreNiveaux;
+
+    public Batiment(int idBatiment, String typeBatiment, int nbreNiveaux) {
+        this.idBatiment = idBatiment;
+        this.typeBatiment = typeBatiment;
+        this.nbreNiveaux = nbreNiveaux;
+    }
+
+    public int getIdBatiment() {
+        return idBatiment;
+    }
+
+    public void setIdBatiment(int idBatiment) {
+        this.idBatiment = idBatiment;
+    }
+
+    public String getTypeBatiment() {
+        return typeBatiment;
+    }
+
+    public void setTypeBatiment(String typeBatiment) {
+        this.typeBatiment = typeBatiment;
+    }
+
+    public int getNbreNiveaux() {
+        return nbreNiveaux;
+    }
+
+    public void setNbreNiveaux(int nbreNiveaux) {
+        this.nbreNiveaux = nbreNiveaux;
+    }
+    
+    
 }
