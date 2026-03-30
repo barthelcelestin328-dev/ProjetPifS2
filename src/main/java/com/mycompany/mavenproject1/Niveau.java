15a3e987a4d49d39ee71c7cef0575b45a5e1b998
cbarthel01 <cbarthel01@pc-0961.csip.in>
2026-03-30 17:18:46 +0200

    Niveau

--- src/main/java/com/mycompany/mavenproject1/Niveau.java
+++ src/main/java/com/mycompany/mavenproject1/Niveau.java
@@ -9,5 +9,39 @@ package com.mycompany.mavenproject1;
  * @author cbarthel01
  */
 public class Niveau {
+    private int idNiveau;
+    private int nbreBatiments;
+    private float hauteurPlafond;
+
+    public Niveau(int idNiveau, int nbreBatiments, float hauteurPlafond) {
+        this.idNiveau = idNiveau;
+        this.nbreBatiments = nbreBatiments;
+        this.hauteurPlafond = hauteurPlafond;
+    }
+
+    public int getIdNiveau() {
+        return idNiveau;
+    }
+
+    public void setIdNiveau(int idNiveau) {
+        this.idNiveau = idNiveau;
+    }
+
+    public int getNbreBatiments() {
+        return nbreBatiments;
+    }
+
+    public void setNbreBatiments(int nbreBatiments) {
+        this.nbreBatiments = nbreBatiments;
+    }
+
+    public float getHauteurPlafond() {
+        return hauteurPlafond;
+    }
+
+    public void setHauteurPlafond(float hauteurPlafond) {
+        this.hauteurPlafond = hauteurPlafond;
+    }
+    
     
 }
