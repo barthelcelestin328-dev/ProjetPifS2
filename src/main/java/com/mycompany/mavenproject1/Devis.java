c58e6f272c32bed3973c55808b0b48e8e3a1da88
cbarthel01 <cbarthel01@pc-0961.csip.in>
2026-03-30 16:55:52 +0200

    premier test

---
+++ src/main/java/com/mycompany/mavenproject1/Devis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author cbarthel01
+ */
+public class Devis {
+
+    public static void main(String[] args) {
+        System.out.println("projet S2");
+    }
+}
