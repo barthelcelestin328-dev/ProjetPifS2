b86332a1c8884e5b49cb3a1f83fbe8d8612dd914
nhaas01 <nhaas01@pc-0960.csip.in>
2026-04-08 10:40:31 +0200

    avancée

--- src/main/java/com/mycompany/mavenproject1/Niveau.java
+++ src/main/java/com/mycompany/mavenproject1/Niveau.java
@@ -3,45 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.mavenproject1;
-
+import java.util.ArrayList;
 /**
  *
  * @author cbarthel01
  */
 public class Niveau {
     private int idNiveau;
-    private int nbreBatiments;
+    private int nbreAppartements;
     private float hauteurPlafond;
+    private ArrayList<Appartement> appartements = new ArrayList<Appartement>();
+    private Immeuble parentImmeuble;
 
-    public Niveau(int idNiveau, int nbreBatiments, float hauteurPlafond) {
+    public Niveau(int idNiveau, int nbreAppartements, float hauteurPlafond, Immeuble parentImmeuble) {
         this.idNiveau = idNiveau;
-        this.nbreBatiments = nbreBatiments;
+        this.nbreAppartements = nbreAppartements;
         this.hauteurPlafond = hauteurPlafond;
+        this.parentImmeuble = parentImmeuble;
     }
 
-    public int getIdNiveau() {
-        return idNiveau;
-    }
-
-    public void setIdNiveau(int idNiveau) {
-        this.idNiveau = idNiveau;
-    }
-
-    public int getNbreBatiments() {
-        return nbreBatiments;
-    }
-
-    public void setNbreBatiments(int nbreBatiments) {
-        this.nbreBatiments = nbreBatiments;
-    }
-
-    public float getHauteurPlafond() {
-        return hauteurPlafond;
-    }
-
-    public void setHauteurPlafond(float hauteurPlafond) {
-        this.hauteurPlafond = hauteurPlafond;
-    }
+    public int getIdNiveau() {return idNiveau;}
+    public void setIdNiveau(int idNiveau) {this.idNiveau = idNiveau;}
+    public int getnbreAppartements() {return nbreAppartements;}
+    public void set(int nbreAppartements) {this.nbreAppartements = nbreAppartements;}
+    public float getHauteurPlafond() {return hauteurPlafond;}
+    public void setHauteurPlafond(float hauteurPlafond) {this.hauteurPlafond = hauteurPlafond;}
     
     
     
